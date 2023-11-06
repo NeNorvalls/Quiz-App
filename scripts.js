bbc8d68e7449600f08ea0df9686462540b3b915b
NeNorvalls <honeygeeh27@gmail.com>
2023-11-06 05:38:56 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -70,4 +70,11 @@ document.addEventListener("DOMContentLoaded", function () {
   
     generateQuiz();
   });
-  
+
+const portfolioURL = 'https://nenorvalls-projects-showcase.netlify.app/';
+
+const goBackButton = document.getElementById('goBackButton');
+
+goBackButton.addEventListener('click', function() {
+  window.location.href = portfolioURL;
+});
